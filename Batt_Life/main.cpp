a10a3506a77f7e3fd5b89512ade2061b3b4b6c06
James Geater <jgeater@outlook.com>
2024-11-19 10:31:51 -0500

    Add project files.

---
+++ Batt_Life/main.cpp
@@ -0,0 +1,39 @@
+﻿#include "pch.h"
+#include <winrt/Windows.Devices.Power.h>
+#include <iostream>
+
+using namespace winrt;
+using namespace Windows::Foundation;
+using namespace Windows::Devices::Power;
+
+int main()
+{
+    init_apartment();
+
+    // Get the default battery
+    Battery battery = Battery::AggregateBattery();
+    BatteryReport report = battery.GetReport();
+
+    // Get the design capacity and full charge capacity
+    auto designCapacity = report.DesignCapacityInMilliwattHours();
+    auto fullChargeCapacity = report.FullChargeCapacityInMilliwattHours();
+
+    // Print the capacities
+    if (designCapacity && fullChargeCapacity)
+    {
+        std::wcout << L"Design Capacity: " << designCapacity.Value() << L" mWh" << std::endl;
+        std::wcout << L"Full Charge Capacity: " << fullChargeCapacity.Value() << L" mWh" << std::endl;
+
+        // Calculate and print the remaining capacity percentage
+        double remainingCapacityPercentage = (static_cast<double>(fullChargeCapacity.Value()) / designCapacity.Value()) * 100;
+        std::wcout << L"Remaining Capacity: " << remainingCapacityPercentage << L"%" << std::endl;
+    }
+    else
+    {
+        std::wcout << L"Unable to retrieve battery capacities." << std::endl;
+    }
+
+    // Existing code
+
+    return 0;
+}
